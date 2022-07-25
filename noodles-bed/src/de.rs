9b465d3d5e410a03c12a0ffe5130163f39e324a5
Marko Malenic <mmalenic1@gmail.com>
2022-07-25 16:41:38 +1000

    Update comments

--- noodles-bed/src/de.rs
+++ noodles-bed/src/de.rs
@@ -6,6 +6,7 @@ use serde::{de, forward_to_deserialize_any, Deserialize, Deserializer};
 use std::io;
 use std::iter::Peekable;
 
+/// A user would likely use a function such as the following to deserialize a set of records.
 pub fn from_bytes<'a, T>(records: &'a [u8]) -> Result<T>
 where
     T: Deserialize<'a>,
@@ -16,15 +17,17 @@ where
     T::deserialize(&mut deserializer)
 }
 
-/// Keeps track of the state associated with the next record that is parsed.
-/// We need to keep track of the state, as records are returned whole from the iterator.
+/// This leeps track of the state associated with the next record that is parsed.
+/// We need to keep track of the state as records are returned whole from the iterator, whereas
+/// the deserializer progresses one field at a time.
 enum RecordState {
     ExpectingChrom,
     ExpectingChromStart(Record<3>),
     ExpectingChromEnd(Record<3>),
 }
 
-/// The Record Deserializer. This struct implements Deserializer and associated Traits.
+/// The Record Deserializer. This struct implements Deserializer and associated Traits. It stores
+/// an iterator over the records that are given to it.
 pub struct Record3Deserializer<I>
 where
     I: Iterator<Item = io::Result<Record<3>>>,
@@ -73,15 +76,23 @@ where
         visitor.visit_seq(self)
     }
 
-    /// We can use forward_to_deserialize_any for many types, as bed Records are self-describing.
+    /// And a sequence, in a similar way.
+    fn deserialize_seq<V>(self, visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        visitor.visit_seq(self)
+    }
+
+    /// We can probably use forward_to_deserialize_any for many types, as bed Records are self-describing.
     forward_to_deserialize_any! {
           bool i8 i16 i32 i64 i128 u8 u16 u32 u64 u128 f32 f64 char str string
-          bytes byte_buf option unit unit_struct newtype_struct seq tuple
+          bytes byte_buf option unit unit_struct newtype_struct tuple
           tuple_struct map enum identifier ignored_any
     }
 }
 
-/// SeqAccess will be needed to sequences, and to deserialize a struct.
+/// SeqAccess will be needed to deserialize sequences and structs.
 impl<'de, I> SeqAccess<'de> for Record3Deserializer<I>
 where
     I: Iterator<Item = io::Result<Record<3>>>,
