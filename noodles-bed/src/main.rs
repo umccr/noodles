aa950188f475998925186da4e3dfe78bd8853c41
Gabriel Simonetto <gabrielfranksimonetto@gmail.com>
2022-09-16 14:21:53 -0300

    wip: unifying types on match

--- noodles-bed/src/main.rs
+++ noodles-bed/src/main.rs
@@ -1,5 +1,5 @@
 use crate::bioserde::{
-    convert_to_format, json_bed_to_noodles_bed, noodles_bed_to_json_bed, SupportedFormat,
+    convert_to_format, json_bed3_to_noodles_bed3, noodles_bed3_to_json_bed3, SupportedFormat,
 };
 use noodles_bed::Record;
 use noodles_core::Position;
@@ -15,6 +15,32 @@ mod bioserde {
     }
 
     pub fn convert_to_format(input: &str, from: SupportedFormat, to: SupportedFormat) -> String {
+        use SupportedFormat::*;
+
+        match (from, to) {
+            // we can later use:
+            //   _ => panic!()
+            // just making it explicit for now.
+            (Record3, Record3) => input.to_string(),
+            (Record3, Record4) => panic!(),
+            (Record3, JsonBed3) => noodles_bed3_to_json_bed3(input),
+            (Record3, JsonBed4) => panic!(),
+            (Record4, Record3) => panic!(),
+            (Record4, Record4) => input.to_string(),
+            (Record4, JsonBed3) => panic!(),
+            (Record4, JsonBed4) => noodles_bed4_to_json_bed4(input),
+            (JsonBed3, Record3) => json_bed3_to_noodles_bed3(input),
+            (JsonBed3, Record4) => panic!(),
+            (JsonBed3, JsonBed3) => input.to_string(),
+            (JsonBed3, JsonBed4) => panic!(),
+            (JsonBed4, Record3) => panic!(),
+            (JsonBed4, Record4) => json_bed4_to_noodles_bed4(input),
+            (JsonBed4, JsonBed3) => panic!(),
+            (JsonBed4, JsonBed4) => input.to_string(),
+        }
+    }
+
+    pub fn bla(input: &str, from: SupportedFormat, to: SupportedFormat) -> String {
         let record = match from {
             SupportedFormat::Record3 => noodles_bed::record_from_str::<Record<3>>(input).unwrap(),
             SupportedFormat::Record4 => noodles_bed::record_from_str::<Record<4>>(input).unwrap(),
@@ -35,7 +61,7 @@ mod bioserde {
     /// A function that receives a string in the noodles-bedrepresentation,
     /// deserialize to a Record<3>, and reserialize to a json-bed representation
     /// TODO: deserialize to a Record<N>
-    pub fn noodles_bed_to_json_bed(input: &str) -> String {
+    pub fn noodles_bed3_to_json_bed3(input: &str) -> String {
         let record: Record<3> = noodles_bed::record_from_str(input).unwrap();
 
         // TODO: start treating errors
@@ -45,10 +71,21 @@ mod bioserde {
     /// A function that receives a string in the json-bed representation,
     /// deserialize to a Record<3>, and reserialize to a noodles-bed representation
     /// TODO: deserialize to a Record<N>
-    pub fn json_bed_to_noodles_bed(input: &str) -> String {
+    pub fn json_bed3_to_noodles_bed3(input: &str) -> String {
         let record: Record<3> = serde_json::from_str(input).unwrap();
 
         // TODO: start treating errors
+        noodles_bed::record_to_string(record).unwrap()
+    }
+
+    // test
+    pub fn noodles_bed4_to_json_bed4(input: &str) -> String {
+        let record: Record<4> = noodles_bed::record_from_str(input).unwrap();
+        serde_json::to_string(&record).unwrap()
+    }
+
+    pub fn json_bed4_to_noodles_bed4(input: &str) -> String {
+        let record: Record<4> = serde_json::from_str(input).unwrap();
         noodles_bed::record_to_string(record).unwrap()
     }
 
@@ -57,25 +94,25 @@ mod bioserde {
         use super::*;
 
         #[test]
-        fn test_json_bed_to_noodles_bed() {
+        fn test_json_bed3_to_noodles_bed3() {
             let input = r#"{"chrom":"sq0","start":8,"end":13}"#;
-            let result = json_bed_to_noodles_bed(input);
+            let result = json_bed3_to_noodles_bed3(input);
 
             let expected = "sq0\t7\t13";
             assert_eq!(&result, expected);
         }
 
         #[test]
-        fn test_noodles_bed_to_json_bed() {
+        fn test_noodles_bed3_to_json_bed3() {
             let input = "sq0\t7\t13";
-            let result = noodles_bed_to_json_bed(input);
+            let result = noodles_bed3_to_json_bed3(input);
 
             let expected = r#"{"chrom":"sq0","start":8,"end":13,"name":null,"score":null,"strand":null,"thick_start":8,"thick_end":13,"color":null,"blocks":[]}"#;
             assert_eq!(&result, expected);
         }
 
         #[test]
-        fn test_json_bed_to_noodles_bed_with_enum_usage() {
+        fn test_json_bed3_to_noodles_bed3_with_enum_usage() {
             let input = r#"{"chrom":"sq0","start":8,"end":13}"#;
             let result =
                 convert_to_format(input, SupportedFormat::JsonBed3, SupportedFormat::Record3);
@@ -85,12 +122,32 @@ mod bioserde {
         }
 
         #[test]
-        fn test_noodles_bed_to_json_bed_with_enum_usage() {
+        fn test_noodles_bed3_to_json_bed3_with_enum_usage() {
             let input = "sq0\t7\t13";
             let result =
                 convert_to_format(input, SupportedFormat::Record3, SupportedFormat::JsonBed3);
 
             let expected = r#"{"chrom":"sq0","start":8,"end":13,"name":null,"score":null,"strand":null,"thick_start":8,"thick_end":13,"color":null,"blocks":[]}"#;
+            assert_eq!(&result, expected);
+        }
+
+        #[test]
+        fn test_json_bed4_to_noodles_bed4_with_enum_usage() {
+            let input = r#"{"chrom":"sq0","start":8,"end":13,"name":"ndls1"}"#;
+            let result =
+                convert_to_format(input, SupportedFormat::JsonBed4, SupportedFormat::Record4);
+
+            let expected = "sq0\t7\t13\tndls1";
+            assert_eq!(&result, expected);
+        }
+
+        #[test]
+        fn test_noodles_bed4_to_json_bed4_with_enum_usage() {
+            let input = "sq0\t7\t13\tndls1";
+            let result =
+                convert_to_format(input, SupportedFormat::Record4, SupportedFormat::JsonBed4);
+
+            let expected = r#"{"chrom":"sq0","start":8,"end":13,"name":"ndls1","score":null,"strand":null,"thick_start":8,"thick_end":13,"color":null,"blocks":[]}"#;
             assert_eq!(&result, expected);
         }
     }
@@ -154,17 +211,17 @@ fn main() {
 
     // From noodles-bed representation to json-bed representation
     let input = r#"{"chrom":"sq0","start":8,"end":13}"#;
-    let result = json_bed_to_noodles_bed(input);
+    let result = json_bed3_to_noodles_bed3(input);
     println!(
-        "json_bed_to_noodles_bed: input: {:?}, result: {:?}",
+        "json_bed3_to_noodles_bed3: input: {:?}, result: {:?}",
         input, result
     );
 
     // From json-bed representation to noodles-bed representation
     let input = "sq0\t7\t13";
-    let result = noodles_bed_to_json_bed(input);
+    let result = noodles_bed3_to_json_bed3(input);
     println!(
-        "noodles_bed_to_json_bed: input: {:?}, result: {:?}",
+        "noodles_bed3_to_json_bed3: input: {:?}, result: {:?}",
         input, result
     );
 
